8947b05565f2d22ad46c62a59630df1f8a737fd5
mateus-s-silva <mattahyah@gmail.com>
2023-03-21 12:44:04 -0300

    e

---
+++ PraticaCpp/beecrowd/led.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+using std::stringstream;
+
+int countLeds(int n){
+    int n
+    
+    
+    
+    
+} 
+ 
+int main() {
+ 
+    
+ 
+    return 0;
+}
---
+++ PraticaCpp/list_01/Q2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+
+// Implemente uma função recursiva que retorne o menor elemento em um vetor
+
+int findSmallestNumber(int vector[]){
+
+
+
+}
+
+int main() {
+ 
+   
+   
+   
+   
+   
+   
+
+ 
+    return 0;
+}
